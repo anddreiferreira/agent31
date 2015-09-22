f2c0196d82035092e324ba5ff984aeacfbbbbfbd
Henrique <hlopes13@hotmail.com>
2015-09-22 14:27:05 -0300

    Initial Commit

---
+++ Agent 31Tests/Agent_31Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Agent_31Tests.swift
+//  Agent 31Tests
+//
+//  Created by Henrique Dutra on 22/09/15.
+//  Copyright (c) 2015 Agent31. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Agent_31Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
