89e99ed3f30be6d5c619ee61e31681f34c05a750
carlosTJunior <carlosjunior117@hotmail.com>
2015-10-26 14:57:19 -0200

    Classe CloudKitHelper com extencoes

---
+++ Agent 31/CloudKitHelper.swift
@@ -0,0 +1,9 @@
+//
+//  CloudKitHelper.swift
+//  Agent 31
+//
+//  Created by carlos alberto teixeira junior on 26/10/15.
+//  Copyright © 2015 Agent31. All rights reserved.
+//
+
+import Foundation
