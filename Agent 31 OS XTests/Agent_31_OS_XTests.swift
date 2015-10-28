a1f00cd8a9b671be5da3b25ddc168170678df259
Artaxexes J. Ferreira <artaxexes@gmail.com>
2015-10-28 15:24:02 -0200

    Scheme para OS X criado

---
+++ Agent 31 OS XTests/Agent_31_OS_XTests.swift
@@ -0,0 +1,36 @@
+//
+//  Agent_31_OS_XTests.swift
+//  Agent 31 OS XTests
+//
+//  Created by Anddrei Artaxexes on 10/28/15.
+//  Copyright © 2015 Agent31. All rights reserved.
+//
+
+import XCTest
+@testable import Agent_31_OS_X
+
+class Agent_31_OS_XTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
