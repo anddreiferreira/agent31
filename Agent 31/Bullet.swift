e82de988f841bbbdc8f722fd743df5a467e9789a
Julio Xavier <juliobepid@hotmail.com>
2015-10-28 17:02:29 -0200

    Início da criação de bala

---
+++ Agent 31/Bullet.swift
@@ -0,0 +1,21 @@
+//
+//  Bullet.swift
+//  Agent 31
+//
+//  Created by Julio Xavier on 28/10/15.
+//  Copyright © 2015 Agent31. All rights reserved.
+//
+
+import SpriteKit
+
+//class Bullet : GameObject {
+//    
+//    let velocity: Int?
+//    let damage: Int?
+//    let ownerGun
+
+//    init(ownerGun: ----CLASSEARMA-----, orientation: Int){
+//        
+//    }
+//    
+//}
