d458ddc85ad83deaf96f5a172b2c7c0400326311
Artaxexes J. Ferreira <artaxexes@gmail.com>
2015-11-09 17:06:15 -0200

    Incremento de currentLevel na funcoes levelUp() de gun e character

--- Agent 31/Function.swift
+++ Agent 31/Function.swift
@@ -17,27 +17,28 @@ func minutesToSeconds(min: Int) -> Double
 // MARK: Gun
 let balanceGunResource = 5.0
 
-func gunLeveUp(gunName: String, gunLevel: Int) -> (timeLevelUp: NSTimeInterval, resourceLevelUp: Int)
+func gunLeveUp(gunName: String, currentGunLevel: Int) -> (timeLevelUp: NSTimeInterval, resourceLevelUp: Int)
 {
+    let nextGunLevel = currentGunLevel + 1
     switch gunName {
         case "gun1":
-            return (Double(gunLevel) * minutesToSeconds(5), gunLevel * Int(pow(balanceGunResource, 1.0)))
+            return (Double(nextGunLevel) * minutesToSeconds(5), nextGunLevel * Int(pow(balanceGunResource, 1.0)))
         case "gun2":
-            return (Double(gunLevel) * minutesToSeconds(10), gunLevel * Int(pow(balanceGunResource, 2.0)))
+            return (Double(nextGunLevel) * minutesToSeconds(10), nextGunLevel * Int(pow(balanceGunResource, 2.0)))
         case "gun3":
-            return (Double(gunLevel) * minutesToSeconds(20), gunLevel * Int(pow(balanceGunResource, 3.0)))
+            return (Double(nextGunLevel) * minutesToSeconds(20), nextGunLevel * Int(pow(balanceGunResource, 3.0)))
         case "gun4", "gun5":
-            return (Double(gunLevel) * minutesToSeconds(30), gunLevel * Int(pow(balanceGunResource, 4.0)))
+            return (Double(nextGunLevel) * minutesToSeconds(30), nextGunLevel * Int(pow(balanceGunResource, 4.0)))
         case "gun6", "gun7":
-            return (Double(gunLevel) * minutesToSeconds(40), gunLevel * Int(pow(balanceGunResource, 5.0)))
+            return (Double(nextGunLevel) * minutesToSeconds(40), nextGunLevel * Int(pow(balanceGunResource, 5.0)))
         case "gun8", "gun9", "gun10":
-            return (Double(gunLevel) * minutesToSeconds(50), gunLevel * Int(pow(balanceGunResource, 6.0)))
+            return (Double(nextGunLevel) * minutesToSeconds(50), nextGunLevel * Int(pow(balanceGunResource, 6.0)))
         case "gun11", "gun12":
-            return (Double(gunLevel) * minutesToSeconds(60), gunLevel * Int(pow(balanceGunResource, 7.0)))
+            return (Double(nextGunLevel) * minutesToSeconds(60), nextGunLevel * Int(pow(balanceGunResource, 7.0)))
         case "gun13", "gun14":
-            return (Double(gunLevel) * minutesToSeconds(70), gunLevel * Int(pow(balanceGunResource, 8.0)))
+            return (Double(nextGunLevel) * minutesToSeconds(70), nextGunLevel * Int(pow(balanceGunResource, 8.0)))
         case "gun15":
-            return (Double(gunLevel) * minutesToSeconds(80), gunLevel * Int(pow(balanceGunResource, 9.0)))
+            return (Double(nextGunLevel) * minutesToSeconds(80), nextGunLevel * Int(pow(balanceGunResource, 9.0)))
         default:
             return (0.00, 0)
     }
@@ -46,9 +47,10 @@ func gunLeveUp(gunName: String, gunLevel: Int) -> (timeLevelUp: NSTimeInterval, 
 // MARK: Character
 let balanceCharacterResource = 10.0
 
-func characterLevelUp(attributeName: String, attributeLevel: Int) -> (timeLevelUp: NSTimeInterval, resourceLevelUp: Int)
+func characterLevelUp(attributeName: String, currentAttributeLevel: Int) -> (timeLevelUp: NSTimeInterval, resourceLevelUp: Int)
 {
-    return (Double(attributeLevel) * minutesToSeconds(30), attributeLevel * Int(pow(balanceCharacterResource, 3.0)))
+    let nextAttributeLevel = currentAttributeLevel + 1
+    return (Double(nextAttributeLevel) * minutesToSeconds(30), nextAttributeLevel * Int(pow(balanceCharacterResource, 3.0)))
 }
 
 // MARK: Gems
