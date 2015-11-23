2ead175bf9eee2c1c2459bfc8d6c69a034b2c8ab
Julio Xavier <juliobepid@hotmail.com>
2015-11-23 17:10:29 -0200

    Adding resources

---
+++ Agent 31/Coin.swift
@@ -0,0 +1,13 @@
+//
+//  Coin.swift
+//  Agent 31
+//
+//  Created by Julio Xavier on 23/11/15.
+//  Copyright © 2015 Agent31. All rights reserved.
+//
+
+import SpriteKit
+
+class Coin: GameObject {
+
+}
