cae0157ddaf25553025265a3f2701dbbfa4955ad
Artaxexes J. Ferreira <artaxexes@gmail.com>
2015-11-24 12:09:59 -0200

    Tela de upgrade de atributos

--- Agent 31/UpgradeLayer.swift
+++ Agent 31/UpgradeLayer.swift
@@ -10,21 +10,28 @@ import SpriteKit
 
 class UpgradeLayer: SKNode
 {
+    var backgroundUpgrade: SKSpriteNode?
+    var attributeName: String?
     var upgradeName: SKLabelNode?
-    var upgradeLevel: SKSpriteNode?
-    var upgradeStars: SKSpriteNode?
-    var animationArea: SKSpriteNode?
+    var upgradeLevel: SKLabelNode?
     var goldAvailableIcon: SKSpriteNode?
     var goldAvailableLabel: SKLabelNode?
+    var cancelButton: SKSpriteNode?
+    var animationArea: SKSpriteNode?
+    var upgradeButton: SKSpriteNode?
     var upgradeGoldCost: SKLabelNode?
     var upgradeDuration: SKLabelNode?
-    var upgradeButton: SKSpriteNode?
     
-    override init() {
+    //override init(name: String) {
+    init(attributeName: String) {
         super.init()
-        loadLabels()
+        
+        loadBackgroundUpgrade()
+        loadHeader(attributeName)
+        loadAnimation()
+        loadTrain()
     }
-
+    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -32,12 +39,31 @@ class UpgradeLayer: SKNode
     //func createLabelNode(text: String, fontName: String = defaultFontName, position: CGPoint = middleOfTheScreenPoint, fontSize: CGFloat = 32.0, color: SKColor = SKColor.whiteColor(), zPosition: CGFloat = 1.0, alignmentMode: SKLabelHorizontalAlignmentMode = SKLabelHorizontalAlignmentMode.Center,name: String) -> SKLabelNode { }
     //func createSpriteNode(imageName: String, position: CGPoint = middleOfTheScreenPoint, anchorPoint: CGPoint = defaultAnchorPoint, scale: CGFloat = 0.5, zPosition: CGFloat = 1.0, name: String) -> SKSpriteNode { }
     
-    func loadLabels() {
-        //upgradeName = createLabelNode()
-        /*
-        goldAvailableLabel = createLabelNode()
-        upgradeGoldCost = createLabelNode()
-        upgradeDuration = createLabelNode()
-        */
+    
+    func loadBackgroundUpgrade() {
+        backgroundUpgrade = createSpriteNode("bgBlur", anchorPoint: CGPointMake(0.5,0), scale: 1.0, zPosition: 2000, position: CGPointMake(0, 0),name: "backgroundUpgrade")
+    }
+    
+    func loadHeader(attributeName: String) {
+        upgradeName = createLabelNode(attributeName, fontName: "CopperplateBlackCondensedSSi", position: CGPoint(x: -middleOfTheScreenPoint.x + 20, y: -middleOfTheScreenPoint.y + 300), fontSize: 30, zPosition: 3000, alignmentMode: SKLabelHorizontalAlignmentMode.Left, name: "upgrade attribute")
+        upgradeLevel = createLabelNode("Level 5", fontName: "CopperplateBlackCondensedSSi", position: CGPoint(x: -middleOfTheScreenPoint.x + 20, y: -middleOfTheScreenPoint.y + 250), fontSize: 15, zPosition: 3000, alignmentMode: SKLabelHorizontalAlignmentMode.Left, name: "upgrade attribute level")
+        //goldAvailableIcon = createSpriteNode()
+        //goldAvailableLabel = createLabelNode()
+        cancelButton = createSpriteNode("exitButton", position: CGPoint(x: -middleOfTheScreenPoint.x + 400, y: -middleOfTheScreenPoint.y + 300), zPosition: 3000, name: "cancelUpgradeButton")
+    }
+    
+    func loadAnimation() {
+        
+    }
+    
+    func loadTrain() {
+        
+    }
+    
+    func putUpgradeLayer() {
+        self.addChild(backgroundUpgrade!)
+        self.addChild(upgradeName!)
+        self.addChild(upgradeLevel!)
+        self.addChild(cancelButton!)
     }
 }
