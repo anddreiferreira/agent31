a46b43c96aae9c454f2a79f0f30c9c2f0666a5f1
Artaxexes J. Ferreira <artaxexes@gmail.com>
2015-12-04 12:03:03 -0200

    Exceptions a repeito de GunsData() funcionando

--- Agent 31/CloudKitExceptions.swift
+++ Agent 31/CloudKitExceptions.swift
@@ -13,13 +13,13 @@ private let _sharedInstance = CloudKitExceptions()
 class CloudKitExceptions : NSObject {
     var characterDataException: Bool
     var resourcesDataException: Bool
-    var gunDataException: Bool
+    var gunsDataException: Bool
     var internetException: Bool
     
     private override init() {
         characterDataException = false
         resourcesDataException = false
-        gunDataException = false
+        gunsDataException = false
         internetException = false
     }
     
