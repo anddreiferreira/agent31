e54a24d288dfa1cc3e77ef7ae428a1ea2fbcb72c
Julio Xavier <juliobepid@hotmail.com>
2015-12-13 22:49:57 -0200

    + Adicionada placeholder de 'door' + Set basic attributes de game object publica + Criada entidade Spawner que será responsável por fazer a geração de inimigos + obs.: Spawner ainda não testada

---
+++ Agent 31/Spawner.swift
@@ -0,0 +1,24 @@
+//
+//  Spawner.swift
+//  Agent 31
+//
+//  Created by Julio Xavier on 13/12/15.
+//  Copyright © 2015 Agent31. All rights reserved.
+//
+
+import SpriteKit
+
+class Spawner: GameObject {
+    
+    init(position: CGPoint) {
+        super.init(imageName: "door", position: position, zPosition: zPositionsCity.BUILDING.rawValue)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func setBasicAttributes() {
+        // Spawner can't have a physics body
+    }
+}
