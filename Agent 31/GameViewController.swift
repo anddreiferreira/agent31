b875447e09645ad235194268f60bafc89c605ce5
carlosTJunior <carlosjunior117@hotmail.com>
2015-12-14 16:35:28 -0200

    Removendo código para feito pra verificar cloudkit

--- Agent 31/GameViewController.swift
+++ Agent 31/GameViewController.swift
@@ -10,50 +10,11 @@ import UIKit
 import SpriteKit
 
 
-//extension SKNode {
-//    class func unarchiveFromFile(file : String) -> SKNode? {
-//        if let path = NSBundle.mainBundle().pathForResource(file, ofType: "sks") {
-//            let sceneData = try! NSData(contentsOfFile: path, options: .DataReadingMappedIfSafe)
-//            let archiver = NSKeyedUnarchiver(forReadingWithData: sceneData)
-//            
-//            archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
-//            let scene = archiver.decodeObjectForKey(NSKeyedArchiveRootObjectKey) as! LaboratoryScene
-//            archiver.finishDecoding()
-//            return scene
-//        } else {
-//            return nil
-//        }
-//    }
-//}
-
 @available(iOS 9.0, *)
 class GameViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-  
-        // Testando o cloudkit
-        
-//        let ckhelper = CloudKitHelper()
-//        ckhelper.saveCharacterProperties(90, speed: 323, shootingRange: 171, shootingPower: 97, backPack: 500, level: 300)
-//        ckhelper.saveResourcesProperties(99, metal: 90, diamond: 80)
-//        ckhelper.saveGunProperties("Kalashnikov", level: 7, price: 200, blocked: false, secret: false, time: 200)
-//        ckhelper.saveGunProperties("AK47", level: 8, price: 100, blocked: false, secret: false, time: 200)
-//        let character = CharacterData()
-//        ckhelper.fetchCharacterProperties(character)
-//        
-//        let delay = 1.0
-//        let time = dispatch_time(DISPATCH_TIME_NOW, Int64(delay * Double(NSEC_PER_SEC)))
-//        dispatch_after(time, dispatch_get_main_queue()) {
-//            print("Character JUMP = \(character.jump)")
-//            print("Character SPEED = \(character.speed)")
-//            print("Character SHOOTINGRANGE = \(character.shootingRange)")
-//            print("Character SHOOTINGPOWER = \(character.shootingPower)")
-//            print("Character BACKPACK = \(character.backPack)")
-//            print("Character LEVEL = \(character.level)")
-//
-//        }
-        
+        super.viewDidLoad()  
     }
 
     override func viewWillAppear(animated: Bool) {
